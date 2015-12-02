ad526e3a534941380ed1a58477697454342374b4
Anais Engelmajer <anaiscorinne@gmail.com>
2015-12-02 14:09:25 -0500

    Made an infinite while loop, commited error for future reference. Need to set parameters to change the variable each time it is printed.

--- Ex_FizzBuzz.rb
+++ Ex_FizzBuzz.rb
@@ -1,13 +1,14 @@
-numbers = (1..100)
-for i in numbers
+i = 1
+while i <= 100
 	if i % 3 == 0 && i % 5 == 0
 		puts "FizzBuzz"
 	elsif i % 5 == 0
 		puts "Buzz"
 	elsif i % 3 == 0
 		puts "Fizz"
-	else
+	else 
 		puts i
 	end
 end
+
 
