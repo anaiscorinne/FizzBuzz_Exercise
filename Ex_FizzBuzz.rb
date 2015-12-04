7d9817036f997e20e9fce47d57688c951dd1a93b
Anais Engelmajer <anaiscorinne@gmail.com>
2015-12-04 16:11:42 -0500

    Iteration 4 completed

--- Ex_FizzBuzz.rb
+++ Ex_FizzBuzz.rb
@@ -1,15 +1,14 @@
 
 (1..100).each do |i|
-	result ""
-	if i % 3 == 0
-		result << "Fizz" 
-	if % 5 == 0
-		result << "Buzz" 
-	if i.to_s[0] == "1"
-		result << "Bang"
-	result << i
+	result = ""
+	result << 'Fizz' if i % 3 == 0
+	result << "Buzz" if % 5 == 0
+	result << "Bang" if i.to_s[0] == "1"
+	if result == ""
+		puts i
+	else puts result
+	end
 end
-puts result
 
 
 
