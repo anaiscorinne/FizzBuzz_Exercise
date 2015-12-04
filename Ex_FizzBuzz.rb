60120dd07217cbcf209e82d96c05d24d01387d95
Anais Engelmajer <anaiscorinne@gmail.com>
2015-12-04 15:37:57 -0500

    Iteration 4 complete but there is a syntax error that is undetermined. Saved for future reference

--- Ex_FizzBuzz.rb
+++ Ex_FizzBuzz.rb
@@ -1,23 +1,15 @@
 
 (1..100).each do |i|
-	if i % 15 == 0 && i.to_s[0] == "1"
-		puts "FizzBuzzBang"
-	elsif i % 5 == 0 && i.to_s[0] == "1"
-		puts "BuzzBang"
-	elsif i % 3 == 0 && i.to_s[0] == "1"
-		puts "FizzBang"
-	elsif i % 15 == 0
-		puts "FizzBuzz"
-	elsif i.to_s[0] == "1"
-		puts "Bang"
-	elsif i % 3 == 0
-		puts "Fizz"
-	elsif i % 5 == 0
-		puts "Buzz"
-	else
-		puts i
-	end
+	result ""
+	if i % 3 == 0
+		result << "Fizz" 
+	if % 5 == 0
+		result << "Buzz" 
+	if i.to_s[0] == "1"
+		result << "Bang"
+	result << i
 end
+puts result
 
 
 
