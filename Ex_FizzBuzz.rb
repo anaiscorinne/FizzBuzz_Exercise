44161b7bd3df1496818b7dfc803e5c8b4b5502c8
Anais Engelmajer <anaiscorinne@gmail.com>
2015-12-04 14:22:43 -0500

    Completed iteration 3 of the exercise

--- Ex_FizzBuzz.rb
+++ Ex_FizzBuzz.rb
@@ -1,19 +1,19 @@
 
 (1..100).each do |i|
-	if (i % 15 == 0) && (i[0].to_s == "1")
+	if i % 15 == 0 && i.to_s[0] == "1"
 		puts "FizzBuzzBang"
-	elsif (i % 5 == 0) && (i[0].to_s == "1")
+	elsif i % 5 == 0 && i.to_s[0] == "1"
 		puts "BuzzBang"
-	elsif (i % 3 == 0) && (i[0].to_s == "1")
+	elsif i % 3 == 0 && i.to_s[0] == "1"
 		puts "FizzBang"
 	elsif i % 15 == 0
 		puts "FizzBuzz"
-	elsif i % 5 == 0
-		puts "Buzz"
-	elsif i[0].to_s == "1"
+	elsif i.to_s[0] == "1"
 		puts "Bang"
 	elsif i % 3 == 0
 		puts "Fizz"
+	elsif i % 5 == 0
+		puts "Buzz"
 	else
 		puts i
 	end
