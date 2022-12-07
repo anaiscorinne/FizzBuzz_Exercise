5768c2f9f8c896b13c54927c0359b49027fad76f
Anais Engelmajer <anaiscorinne@gmail.com>
2022-12-06 21:03:29 -0500

    Fix syntax error
    Added i to line 5

--- Ex_FizzBuzz.rb
+++ Ex_FizzBuzz.rb
@@ -2,7 +2,7 @@
 (1..100).each do |i|
 	result = ""
 	result << 'Fizz' if i % 3 == 0
-	result << "Buzz" if % 5 == 0
+	result << "Buzz" if i % 5 == 0
 	result << "Bang" if i.to_s[0] == "1"
 	if result == ""
 		puts i
